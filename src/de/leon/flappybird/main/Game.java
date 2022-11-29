98134bd3fc411e36de5106b2a69d1075029443c3
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-29 19:15:08 +0000

    first commit

--- src/de/leon/flappybird/main/Game.java
+++ src/de/leon/flappybird/main/Game.java
@@ -1,5 +1,13 @@
 package de.leon.flappybird.main;
 
-public class Game {
+public class Game extends Canvas implements Runnable {
     
+    public static final int WIDTH = 432;    
+    public static final int HEIGHT = 768;
+
+    public static void main(String[] args) {
+        
+    }
+
+
 }
