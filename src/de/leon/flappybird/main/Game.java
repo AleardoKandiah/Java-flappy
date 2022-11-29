915bf9e3bb41a21b6a17b4bef5a07a49a8e781fb
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-29 19:21:37 +0000

    first commit

--- src/de/leon/flappybird/main/Game.java
+++ src/de/leon/flappybird/main/Game.java
@@ -1,12 +1,45 @@
 package de.leon.flappybird.main;
+import java.awt.Canvas;
 
+SuppressWarnings("serial")
 public class Game extends Canvas implements Runnable {
     
     public static final int WIDTH = 432;    
     public static final int HEIGHT = 768;
 
     public static void main(String[] args) {
-        
+
+    }
+
+    public synchronized void start() {
+
+    }
+    
+    public void init() {
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
+
+    @Override
+    public void run() {
+        init();
+        this.requestFocus();
+
+        long pastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000/ amountOfTicks;
+        double delta;
+        long timer = System.currentTimeMillis();
+        int updates;
+        int frames;
+
     }
 
 
