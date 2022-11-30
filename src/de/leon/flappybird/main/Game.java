0a46d69d17aed8910afdb8476d55cc37d4d5e127
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-30 18:35:02 +0000

    Game Canvas

--- src/de/leon/flappybird/main/Game.java
+++ src/de/leon/flappybird/main/Game.java
@@ -14,6 +14,9 @@ public class Game extends Canvas implements Runnable {
 	public static BufferedImage img_gameover;
 	public static BufferedImage background;
 	public static Ground ground;
+	public static Bird bird;
+
+
 
     public static void main(String[] args) {
 
