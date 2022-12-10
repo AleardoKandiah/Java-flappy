a847ca3d8f2ba1437215e7a4ae4398d4e92a9dc6
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-12-10 18:06:26 +0000

    Graphics loaders

--- src/de/leon/flappybird/loaders/GraphicsLoader.java
+++ src/de/leon/flappybird/loaders/GraphicsLoader.java
@@ -1,5 +1,21 @@
 package de.leon.flappybird.loaders;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class GraphicsLoader {
-    
+	
+	public static BufferedImage loadGraphics(String path) {
+		BufferedImage image = null;
+		
+		try {
+			image = ImageIO.read(ResourceLoader.load("/" + path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return image;
+	}
 }
