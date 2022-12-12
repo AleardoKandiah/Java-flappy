87a78418de67c16d314a28d01da78e2bc1e2c161
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-12-12 18:29:56 +0000

    Key hadnler

--- src/de/leon/handlers/KeyHandler.java
+++ src/de/leon/handlers/KeyHandler.java
@@ -3,3 +3,29 @@ package de.leon.handlers;
 public class KeyHandler {
     
 }
+package de.leon.flappybird.handlers;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import de.leon.flappybird.main.Game;
+
+public class KeyHandler implements KeyListener {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_SPACE) {
+			Game.bird.setVelY(-5);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		
+	}
+}
