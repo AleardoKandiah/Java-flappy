4f916eaa2ab7047529d7d6cc2827e8cf496b5a24
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-12-17 12:21:55 +0000

    Object handler

--- src/de/leon/handlers/ObjectHandler.java
+++ src/de/leon/handlers/ObjectHandler.java
@@ -1,3 +1,37 @@
+package de.leon.flappybird.handlers;
+
+import java.awt.Graphics;
+import java.util.LinkedList;
+
+import de.leon.flappybird.supers.GameObject;
+
 public class ObjectHandler {
-    
+
+	public static LinkedList<GameObject> list = new LinkedList<GameObject>();
+
+	public static void addObject(GameObject o) {
+		list.add(o);
+	}
+
+	public static void removeObject(GameObject o) {
+		list.remove(o);
+	}
+
+	public static void render(Graphics g) {
+		GameObject temp = null;
+
+		for (int i = 0; i < list.size(); i++) {
+			temp = list.get(i);
+			temp.render(g);
+		}
+	}
+	
+	public static void tick() {
+		GameObject temp = null;
+
+		for (int i = 0; i < list.size(); i++) {
+			temp = list.get(i);
+			temp.tick();
+		}
+	}
 }
