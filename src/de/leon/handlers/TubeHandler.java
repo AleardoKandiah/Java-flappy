76036069d084adaa2b8de07b2acb7e1ca37eeac0
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-12-24 16:34:31 +0000

    Object handler

--- src/de/leon/handlers/TubeHandler.java
+++ src/de/leon/handlers/TubeHandler.java
@@ -5,7 +5,6 @@ import de.leon.flappybird.enums.TubeType;
 import de.leon.flappybird.gameobjects.Tube;
 import de.leon.flappybird.main.Game;
 
-
 public class TubeHandler {
 
 	private static Random random = new Random();
